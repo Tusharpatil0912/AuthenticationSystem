c4c442dc99f63bce907d68d83879ed37858dc8a2
TUSHAR PATIL <88529988+Tusharpatil0912@users.noreply.github.com>
2025-05-15 16:56:09 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `https://authentication-system-one.vercel.app/auth/signup`;
+            const url = `https://authentication-system-rust-two.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
